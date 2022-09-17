1ec28d9e2e0e3f54cb157e2b201e71de288267b7
jannpf <63592451+jannpf@users.noreply.github.com>
2022-09-17 17:54:46 +0200

    Add route length calculation function

--- data_models/Route.js
+++ data_models/Route.js
@@ -1,7 +1,6 @@
 import Point from "./Point.js";
 
 export default class Route {
-    #temperature = 0;
     #points = [];
     #distanceMatrix = [];
 
@@ -16,21 +15,30 @@ export default class Route {
         if (!typeof (distanceMatrix) === Array || distanceMatrix.lenth <= 0 || !distanceMatrix.every(x => typeof (x) === Number))
             throw new Error(`Invalid Argument: Expected non-empty list of floats for distance matrix`);
         this.#points = points;
-        this.#distanceMatrix = distanceMatrix
+        this.#distanceMatrix = distanceMatrix;
     }
 
     get points() {
         return this.#points;
     }
 
-    get length() {
-        throw new Error("Not Implemented");
+    /**
+     * Calculate total length of route based on the distance matrix
+     */
+    getLength() {
+        length = 0
+        for (let i = 1; i < this.#points.length(); i++) {
+            p1 = this.#points[i-1];
+            p2 = this.#points[i];
+            length += this.#distanceMatrix.get(p1.id).get(p2.id);
+        }
+        return length;
     }
 
     swapPoints(i1, i2) {
         var temp = this.#points[i1];
         this.#points[i1] = this.#points[i2];
         this.#points[i2] = temp;
-        return this.#points
+        return this.#points;
     }
 }
