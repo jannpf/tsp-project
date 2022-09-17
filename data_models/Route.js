26fe82c7c867a12b521ab8138d622e2d8dc84648
jannpf <63592451+jannpf@users.noreply.github.com>
2022-09-17 16:48:37 +0200

    Add point swapping function

--- data_models/Route.js
+++ data_models/Route.js
@@ -1,30 +1,34 @@
 class Route {
     #temperature = 0;
-    #points = points;
+    #points = [];
+    #distanceMatrix = [];
 
     /**
      * 
      * @param {Array} points 
+     * @param {Array} distanceMatrix
      */
-    constructor(points) {
+    constructor(points, distanceMatrix) {
         if (!typeof (points) === Array || points.lenth <= 0 || !points.every(x => typeof (x) === Point))
             throw new Error(`Invalid Argument: Expected list of points`);
-
+        if (!typeof (distanceMatrix) === Array || distanceMatrix.lenth <= 0 || !distanceMatrix.every(x => typeof (x) === Number))
+            throw new Error(`Invalid Argument: Expected non-empty list of floats for distance matrix`);
         this.#points = points;
+        this.#distanceMatrix = distanceMatrix
     }
 
     get points() {
         return this.#points;
     }
 
-    get temperature() {
-        return this.#temperature;
+    get length() {
+        throw new Error("Not Implemented");
     }
 
-    set temperature(value) {
-        if (value < 0)
-            throw new Error(`Invalid Argument: Temperature must be > 0`);
-
-        this.#temperature = value;
+    swapPoints(i1, i2) {
+        var temp = this.#points[i1];
+        this.#points[i1] = this.#points[i2];
+        this.#points[i2] = temp;
+        return this.#points
     }
 }
