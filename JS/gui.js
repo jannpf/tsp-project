81043022e724674067803a1b65742eb5c9125a15
domin <dominik@familie-eder.de>
2022-09-17 10:13:49 +0200

    -draw_point also takes coordinates

--- JS/gui.js
+++ JS/gui.js
@@ -26,24 +26,26 @@ function getMousePos(canvas, evt) {
     };
 }
 
-function draw_point(evt) {
 
+function draw_point(evt, Point) {
 
-    var pos = getMousePos(canvas, evt);
+    if (evt !== 0) {
+        var Point = getMousePos(canvas, evt);
+        Point.number = 12;
+    }
 
     //marker with number inside
     ctx.fillStyle = "#263238"
     let p = new Path2D('M5 0h20c2.76 0 5 2.22 5 4.97v19.86c0 2.74-2.24 4.96-5 4.96h-5.63L15 36l-4.38-6.2H5c-2.76 0-5-2.23-5-4.97V4.97C0 2.22 2.24 0 5 0Z');
-    ctx.setTransform(1, 0, 0, 1, pos.x, pos.y);
+    ctx.setTransform(1, 0, 0, 1, Point.x, Point.y);
     ctx.fill(p);
 
     ctx.fillStyle = "#FFFFFF"
     ctx.font = '20px poppins';
-    ctx.fillText('12', 6, 21);
+    ctx.fillText(Point.number, 6, 21);
 
 }
 
 function start_algorithm() {
-
     alert("started")
 }
