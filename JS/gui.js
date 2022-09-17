2c4c23ef27ac1b47497ba1cb92a23f69ef1b893d
domin <dominik@familie-eder.de>
2022-09-17 09:15:59 +0200

    -Added Preliminary nonrepsonsive gui for testing

---
+++ JS/gui.js
@@ -0,0 +1,49 @@
+
+
+function initiate_canvas(){
+
+    canvas = document.getElementById("canvas");
+    if (canvas.getContext) {
+        ctx = canvas.getContext("2d");
+
+        canvas.width = window.innerWidth*0.96;
+        canvas.height = window.innerHeight*0.6;
+
+        return ctx;
+      }
+    }
+
+
+window.addEventListener('resize', () => {
+    initiate_canvas();
+  })
+
+function getMousePos(canvas, evt) {
+    var rect = canvas.getBoundingClientRect();
+    return {
+        x: evt.clientX - rect.left,
+        y: evt.clientY - rect.top,
+    };
+}
+
+function draw_point(evt) {
+
+
+    var pos = getMousePos(canvas, evt);
+
+    //marker with number inside
+    ctx.fillStyle = "#263238"
+    let p = new Path2D('M5 0h20c2.76 0 5 2.22 5 4.97v19.86c0 2.74-2.24 4.96-5 4.96h-5.63L15 36l-4.38-6.2H5c-2.76 0-5-2.23-5-4.97V4.97C0 2.22 2.24 0 5 0Z');
+    ctx.setTransform(1, 0, 0, 1, pos.x, pos.y);
+    ctx.fill(p);
+
+    ctx.fillStyle = "#FFFFFF"
+    ctx.font = '20px poppins';
+    ctx.fillText('12', 6, 21);
+
+}
+
+function start_algorithm() {
+
+    alert("started")
+}
