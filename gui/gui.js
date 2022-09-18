6613bc57c54d9c139992949e5b45452a2a5978dc
domin <dominik@familie-eder.de>
2022-09-19 01:30:27 +0200

    -added csv upload modal

--- gui/gui.js
+++ gui/gui.js
@@ -1,34 +1,41 @@
 import Parameters from "../data_models/Parameters.js";
 import Point from "../data_models/Point.js";
 
+/**
+ * Initiates JS-controlled Elements on window-load
+ */
 window.addEventListener('load', () => {
-    initiate();
+    initiate_canvas();
+    change_slider();
+
+    return window.import_modal = document.getElementById("import-modal");
 })
 
+/**
+ * Re-Scales the Canvas on window resize
+ */
 window.addEventListener('resize', () => {
     initiate_canvas();
 })
 
+
+
+/**
+ * Handles Modal closing
+ */
+document.addEventListener('click', function handleClickOutsideModal(event) {
+    window.import_tab = document.getElementById("import-tab");
+    if (!import_modal.contains(event.target) && !import_tab.contains(event.target)) {
+        import_modal.style.display = 'none';
+    }
+});
+
 const parameters = new Parameters();
 
-window.initiate = function initiate() {
-    initiate_canvas();
-    change_slider();
-}
-
-window.change_slider = function change_slider(fixed) {
-
-    const mySlider = document.getElementById("my-slider");
-    const sliderValue = document.getElementById("slider-value");
-
-    if (fixed == "text") {
-        sliderValue.value > 100 ? sliderValue.value = mySlider.value = 100 : (sliderValue.value < 0 ? sliderValue.value = mySlider.value = 0 : mySlider.value = sliderValue.value);
-    } else {
-        sliderValue.value = mySlider.value;
-    }
-}
-
-
+/**
+ * Scales the Canvas to correct size
+ * @returns {Context2D} canvas interaction object
+ */
 function initiate_canvas() {
 
     window.canvas = document.getElementById("canvas");
@@ -41,11 +48,28 @@ function initiate_canvas() {
         return ctx;
     }
 }
+/**
+ * Syncs Slider Text and Bar two
+ * @param {String} fixed fixed="text" sets the text as truth
+ */
+window.change_slider = function change_slider(fixed) {
 
+    const mySlider = document.getElementById("my-slider");
+    const sliderValue = document.getElementById("slider-value");
 
+    if (fixed == "text") {
+        sliderValue.value > 100 ? sliderValue.value = mySlider.value = 100 : (sliderValue.value < 0 ? sliderValue.value = mySlider.value = 0 : mySlider.value = sliderValue.value);
+    } else {
+        sliderValue.value = mySlider.value;
+    }
+}
 
-
-
+/**
+ * gets current location of the mouse on the canvas
+ * @param {Canvas} canvas the main canvas
+ * @param {onmousedown} evt the mouse position
+ * @returns {Object} x, y
+ */
 function getMousePos(canvas, evt) {
     var rect = canvas.getBoundingClientRect();
     return {
@@ -54,8 +78,10 @@ function getMousePos(canvas, evt) {
     };
 }
 
-
-
+/**
+ * Draws a point p onto the canvas with the p.id as a label
+ * @param {Point} p 
+ */
 window.draw_point = function draw_point(p) {
     if (!typeof (p) == Point)
         throw new Error(`Invalid Argument: Expected type 'Point' but got '${typeof (p)}'`);
@@ -70,7 +96,10 @@ window.draw_point = function draw_point(p) {
     ctx.fillText(p.id, 3, 21);
     ctx.resetTransform();
 }
-
+/**
+ * Draws all Points stored in a Parameters onto the canvas
+ * @param {Parameters} param 
+ */
 function draw_parameters_points(param) {
     if (!typeof (param) == Parameters) {
         throw new Error(`Invalid Argument: Expected type 'Paramters' but got '${typeof (param)}'`);
@@ -84,6 +113,13 @@ function draw_parameters_points(param) {
         draw_point(p);
     });
 }
+
+/**
+ * Adds a new Point at the location of the cursor.
+ * Existing Points at the location are deleted
+ * manages Points in Parameters
+ * @param {onmousedown} evt the mouse lovation
+ */
 
 window.click_canvas = function click_canvas(evt) {
 
@@ -115,7 +151,12 @@ window.click_canvas = function click_canvas(evt) {
     draw_parameters_points(parameters);
     console.log(parameters.points)
 }
-
+/**
+ * Calculates the distance between two Points
+ * @param {Point} a 
+ * @param {Point} b 
+ * @returns {Number} Distance in Pixel
+ */
 function get_distance_two_points(a, b) {
     if (!typeof (a) == !typeof (b) == Point)
         throw new Error(`Invalid Argument: Expected type 'Point' but got '${typeof (p)}'`);
@@ -123,8 +164,26 @@ function get_distance_two_points(a, b) {
     return Math.hypot(a.x - b.x, a.y - b.y);
 }
 
-
-
+/**
+ * Starts Algorithm
+ */
 window.start_algorithm = function start_algorithm() {
     alert("started")
+}
+
+function initiate_modals() {
+    // Get the modal
+
+
+
+}
+
+window.import_file = function import_file(evt) {
+
+    import_modal.style.display = "flex";
+}
+window.close_import = function close_import(evt) {
+
+    window.close_modal = document.getElementsByClassName("close-modal");
+    import_modal.style.display = "none";
 }
