59bc271600464df2ca4eef8e8e607a073f894186
jannpf <63592451+jannpf@users.noreply.github.com>
2022-09-23 10:44:02 +0200

    Fix lat/lon assignment

--- data_models/Point.js
+++ data_models/Point.js
@@ -31,18 +31,18 @@ export default class Point {
      * using the haversine formula
      * @param {Point} dest 
      */
-     getDistance(dest) {
-        let lon1 = this.#x
-        let lat1 = this.#y
-        let lon2 = dest.x
-        let lat2 = dest.y
+    getDistance(dest) {
+        let lon1 = this.#y
+        let lat1 = this.#x
+        let lon2 = dest.y
+        let lat2 = dest.x
 
         // radius of the earth in km
         let r = 6371
         let p = Math.PI / 180
 
         // haversine formula
-        let a = 0.5 - Math.cos((lat2-lat1)*p)/2 + Math.cos(lat1*p)*Math.cos(lat2*p) * (1-Math.cos((lon2-lon1)*p)) / 2
+        let a = 0.5 - Math.cos((lat2 - lat1) * p) / 2 + Math.cos(lat1 * p) * Math.cos(lat2 * p) * (1 - Math.cos((lon2 - lon1) * p)) / 2
         return 2 * r * Math.asin(Math.sqrt(a))
     }
 
